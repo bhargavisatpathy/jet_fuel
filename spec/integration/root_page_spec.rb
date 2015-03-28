8eb730b2e2e64cf5a03f47e3cd004e0a392e4016
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2015-03-28 15:22:07 -0600

    completed stats page

--- spec/integration/root_page_spec.rb
+++ spec/integration/root_page_spec.rb
@@ -13,24 +13,27 @@ describe "anonymous user", type: :feature do
     visit new_url_path
     fill_in "url[long]", with: long_url
     click_link_or_button("Shorten")
-    expect(page).to have_content("#{long_url}")
+    expect(page).to have_content(long_url)
     visit new_url_path
     fill_in "url[long]", with: another_long_url
     click_link_or_button("Shorten")
-    expect(page).to have_content("#{another_long_url}")
+    expect(page).to have_content(another_long_url)
   end
 
-  xit "can see the list of urls sorted by popularity" do
-    long_url = "http://hubba-hubba-hubstub.herokuapp.com"
-    another_long_url = "http://maps.google.com"
-    visit new_url_path
-    fill_in "url[long]", with: long_url
-    click_link_or_button("Shorten")
-    expect(page).to have_content("#{long_url}")
-    visit new_url_path
-    fill_in "url[long]", with: another_long_url
-    click_link_or_button("Shorten")
-    expect(page).to have_content("#{another_long_url}")
+  it "can see the list of urls sorted by popularity" do
+    url = create(:url, long: "http://www.gmail.com", popularity: 3)
+    url1 = create(:url)
+    visit root_path
+    within("tr:first") do
+      expect(page).to have_content(url1.long)
+    end
+    click_link_or_button("by popularity")
+    within("tr:first") do
+      expect(page).to have_content(url.long)
+    end
+    within("tr:last") do
+      expect(page).to have_content(url1.long)
+    end
   end
 
   it "can see the list of urls sorted by age" do
@@ -42,12 +45,24 @@ describe "anonymous user", type: :feature do
     visit new_url_path
     fill_in "url[long]", with: another_long_url
     click_link_or_button("Shorten")
-    expect(page).to have_content("List of urls")
-    within("li:first") do
-      expect(page).to have_content("#{another_long_url}")
+    expect(page).to have_content("List Of URLs")
+    within("tr:first") do
+      expect(page).to have_content(another_long_url)
     end
-    within("li:last") do
-      expect(page).to have_content("#{long_url}")
+    within("tr:last") do
+      expect(page).to have_content(long_url)
     end
+  end
+
+  it "can visit to statistics page" do
+    long_url = "http://maps.google.com"
+    visit new_url_path
+    fill_in "url[long]", with: long_url
+    click_link_or_button("Shorten")
+    click_link_or_button(long_url)
+    expect(page).to have_content(long_url)
+    expect(page).to have_content("Short Alias:")
+    expect(page).to have_content("Visits:")
+    expect(page).to have_content("Created At:")
   end
 end
