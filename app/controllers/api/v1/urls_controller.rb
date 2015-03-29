5d3391af819824dc88804d1545edf4855e2531f7
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2015-03-28 19:03:11 -0600

    building api

---
+++ app/controllers/api/v1/urls_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::UrlsController < ApplicationController
+  respond_to :json
+
+  def create
+    @url = Url.find_or_create_by(url_params)
+    if @url.save
+      response = { url: { long: @url.long, short: @url.short_url }, success: true }
+      render json: response, status: 201
+    else
+      response = { success: false, message: "#{@url.errors.full_messages}" }
+      render json: response, status: 500
+    end
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:long)
+  end
+end
